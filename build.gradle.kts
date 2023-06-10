ecc2293717fac858e8fb2e11389589638ed17013
68542998+GreenpeaceKT@users.noreply.github.com <68542998+GreenpeaceKT@users.noreply.github.com>
2023-06-10 19:49:58 +0900

    fix updater

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,8 @@ subprojects {
 
     aliucord {
         author("GreenpeaceKT", 644526522811547659L)
-        updateUrl.set("https://raw.githubusercontent.com/GreenpeaceKT/bettercodeBlocks/builds/updater.json")
-        buildUrl.set("https://raw.githubusercontent.com/GreenpeaceKT/bettercodeBlocks/builds/%s.zip")
+        updateUrl.set("https://raw.githubusercontent.com/GreenpeaceKT/Aliucord-plugins/builds/updater.json")
+        buildUrl.set("https://raw.githubusercontent.com/GreenpeaceKT/Aliucord-plugins/builds/%s.zip")
     }
 
     android {
